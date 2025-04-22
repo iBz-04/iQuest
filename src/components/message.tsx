2bb99b10e5b976a3607cc2cdd4946634deb54fa9
iBz-04 <issakaibrahimrayamah@gmail.com>
2025-04-22 13:07:33 +0300

    feat: add branding

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Markdown from "markdown-to-jsx";
 import cx from "@/utils/cx";
 import { Message as MessageProps } from "ai/react";
-import UpstashLogo from "@/components/upstash-logo";
+import Image from 'next/image';
 import { IconUser } from "@tabler/icons-react";
 
 const Message: React.FC<MessageProps> = ({ content, role }) => {
@@ -48,12 +48,22 @@ const Avatar: React.FC<{ isUser?: boolean; className?: string }> = ({
   return (
     <div
       className={cx(
-        "flex items-center justify-center size-8 shrink-0 rounded-full",
+        "flex items-center justify-center size-8 shrink-0 rounded-full overflow-hidden",
         isUser ? "bg-black text-gray-300" : "bg-black border border-zinc-800",
         className,
       )}
     >
-      {isUser ? <IconUser size={20} /> : <UpstashLogo />}
+      {isUser ? (
+        <IconUser size={20} />
+      ) : (
+        <Image 
+          src="/ele.png" 
+          alt="iQuest logo" 
+          width={32}
+          height={32}
+          className="object-cover w-full h-full"
+        />
+      )}
     </div>
   );
 };
--- src/components/powered-by.tsx
+++ src/components/powered-by.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Link from "next/link";
-import UpstashLogo from "@/components/upstash-logo";
+import Image from 'next/image';
 
 const PoweredBy: React.FC = () => {
   return (
     <div className="flex justify-center mt-3">
       <div className="flex items-center gap-1 text-xs text-gray-500">
-        <span>Powered by</span>
+        <span>Developed by</span>
         <Link
-          href="https://github.com/iBz-04/iQuest"
+          href="https://github.com/OmniS0FT/iQuest"
           target="_blank"
           className="flex items-center gap-1 decoration-transparent hover:decoration-transparent hover:text-gray-300 hover:bg-transparent"
         >
-          <UpstashLogo size={14} />
-          <span>Upstash</span>
+          <Image 
+            src="/ele.png" 
+            alt="Powered by logo" 
+            width={14}
+            height={14}
+            className="inline-block"
+          />
+          <span>Omnisoft</span>
         </Link>
       </div>
     </div>
